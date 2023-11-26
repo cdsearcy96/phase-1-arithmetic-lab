469f3e472735e517c0ec986d2292013ec8761a6d
+ <+>
2023-11-26 13:56:53 -0800

    added 4 arithmetic variables

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+multiply;
 
-// Write your code here
+// Creates a variable called random that will generate a random integer greater than 0.
+let random = Math.floor(Math.random() *100) + 1;
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+const num3 = 4;
+const num4 = 9;
+let mod = num3 % num4;
+mod;
+// Creates a variable called max that finds the highest number in a set; the value returned should be 20.
+
+let max = Math.max(5,15,17,20);
+max;
